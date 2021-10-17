fb849550b38b661fab341b0c9c14a9ecab46f042
SHINU VARGHESE <shinushinu01997@gmail.com>
2021-10-17 21:05:25 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done) {
-    const url = "mongodb+srv://shinu:shinu01997@crud.usdgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    const url = "mongodb+srv://shinu:shinu01997@crud.usdgu.mongodb.net/usermanagement?retryWrites=true&w=majority"
     const dbname='usermanagement'
 
     mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
